73943837cd9879de86b87cc13831a1768d5ef0d3
ejuniorls <ejuniorls@outlook.com>
2021-08-20 00:29:16 -0300

    adicionando rotas

---
+++ src/Controller/Persistencia.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace Alura\Cursos\Controller;
+
+
+use Alura\Cursos\Entity\Curso;
+use Alura\Cursos\Infra\EntityManagerCreator;
+
+class Persistencia implements InterfaceControladorRequisicao
+{
+    /**
+     * @var \Doctrine\ORM\EntityManagerInterface
+     */
+
+    public function __construct()
+    {
+        $this->entityManager = (new EntityManagerCreator())
+            ->getEntityManager();
+    }
+
+    public function processaRequisicao(): void
+    {
+        // pegar dados do formulário e filtra
+        $descricao = filter_input(
+            INPUT_POST,
+            'descricao',
+            FILTER_SANITIZE_STRING
+        );
+
+        // montar modelo curso
+        $curso = new Curso();
+        $curso->setDescricao($descricao);
+
+        // inserir no banco
+        $this->entityManager->persist($curso);
+        $this->entityManager->flush();
+
+        header('Location: /listar-cursos', false, 302);
+    }
+}
--- view/cursos/formulario-novo-curso.php
+++ view/cursos/formulario-novo-curso.php
@@ -1,4 +1,5 @@
-<?php include __DIR__ . '/../inicio-html.php'; ?>    <form action="">
+<?php include __DIR__ . '/../inicio-html.php'; ?>
+    <form action="/salvar-curso" method="post">
         <div class="form-group">
             <label for="descricao">Descrição</label>
             <input type="text" id="descricao" name="descricao" class="form-control">
