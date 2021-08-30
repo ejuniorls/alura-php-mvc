47dfed44b1ea176aa835b0743a29eded48e31eb7
ejuniorls <ejuniorls@outlook.com>
2021-08-29 21:54:15 -0300

    web service

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use Alura\Cursos\Controller\CursosEmJson;
+use Alura\Cursos\Controller\CursosEmXml;
 use Alura\Cursos\Controller\Exclusao;
 use Alura\Cursos\Controller\FormularioEdicao;
 use Alura\Cursos\Controller\FormularioInsercao;
@@ -18,4 +20,6 @@ return [
     '/login' => FormularioLogin::class,
     '/realiza-login' => RealizaLogin::class,
     '/logout' => Deslogar::class,
+    '/buscarCursosEmJson' => CursosEmJson::class,
+    '/buscarCursosEmXml' => CursosEmXml::class
 ];
